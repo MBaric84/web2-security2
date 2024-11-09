0767de831b5753ed6c696ce94cd87702942466d7
Marko Barić <markob0804@gmail.com>
2024-11-09 12:09:31 +0100

    prominia broj

--- web-security-backend/app.js
+++ web-security-backend/app.js
@@ -7,7 +7,7 @@ const dataExposureRoutes = require('./routes/dataExposure');
 const app = express();
 const PORT=process.env.PORT || 3000;
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: 'http://localhost:5000' }));
 app.use(helmet());
 app.use(express.json());
 
