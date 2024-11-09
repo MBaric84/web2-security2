c40abd9db9914fec7039b70133c8a085bee2cc97
Marko Barić <markob0804@gmail.com>
2024-11-09 12:38:47 +0100

    minja svasta idk

--- web-security-frontend/src/components/DataExposureDemo.js
+++ web-security-frontend/src/components/DataExposureDemo.js
@@ -8,7 +8,7 @@ const DataExposureDemo = () => {
 
   const fetchVulnerableData = async () => {
     try {
-      const response = await axios.get('${apiUrl}/data-exposure/vulnerable');
+      const response = await axios.get(`${apiUrl}/data-exposure/vulnerable`);
       setVulnerableData(response.data);
     } catch (error) {
       console.error("Error fetching vulnerable data", error);
@@ -17,7 +17,7 @@ const DataExposureDemo = () => {
 
   const fetchSafeData = async () => {
     try {
-      const response = await axios.get('${apiUrl}/data-exposure/safe');
+      const response = await axios.get(`${apiUrl}/data-exposure/safe`);
       setSafeData(response.data);
     } catch (error) {
       console.error("Error fetching safe data", error);
