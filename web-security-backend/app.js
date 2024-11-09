676336b21b4d9eb38d7c9fbc5d2c99c891f0f6ac
Marko Barić <markob0804@gmail.com>
2024-11-09 11:14:00 +0100

    idk

---
+++ web-security-backend/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const xssRoutes = require('./routes/xss');
+const dataExposureRoutes = require('./routes/dataExposure');
+
+const app = express();
+const PORT=process.env.PORT || 3000;
+
+app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(helmet());
+app.use(express.json());
+
+app.use('/xss', xssRoutes);
+app.use('/data-exposure', dataExposureRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
