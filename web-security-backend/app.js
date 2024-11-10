170afdc208b742a330dd3f6b6249b59b1b8ce155
Marko Barić <markob0804@gmail.com>
2024-11-10 15:21:06 +0100

    evo valjda radi sa renderon

--- web-security-backend/app.js
+++ web-security-backend/app.js
@@ -7,7 +7,7 @@ const dataExposureRoutes = require('./routes/dataExposure');
 const app = express();
 const PORT=process.env.PORT || 5000;
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: 'https://web2-security2-1.onrender.com' }));
 app.use(helmet());
 app.use(express.json());
 
